a3b4798d071489e16814ab8fa1dbbc5036f35349
ranhaowen <ranhaowen@wingtech.com>
2019-03-14 09:31:54 +0800

    v1.1.1

--- app/src/main/java/com/example/ranhaowen/hellowworld/MainActivity.java
+++ app/src/main/java/com/example/ranhaowen/hellowworld/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.ranhaowen.hellowworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreat()", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreat()", Toast.LENGTH_SHORT).show();
     }
 }
