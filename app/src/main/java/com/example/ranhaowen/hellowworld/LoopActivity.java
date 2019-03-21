48b0043e80f9a9a73991a7861ad2409a40ae0fb4
ranhaowen <ranhaowen@wingtech.com>
2019-03-21 10:54:15 +0800

    First stup

--- app/src/main/java/com/example/ranhaowen/hellowworld/LoopActivity.java
+++ app/src/main/java/com/example/ranhaowen/hellowworld/LoopActivity.java
@@ -1,13 +1,18 @@
 package com.example.ranhaowen.hellowworld;
 
+import android.os.Handler;
+import android.os.Message;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
+
+import static java.lang.System.exit;
 
 public class LoopActivity extends AppCompatActivity {
 
@@ -17,14 +22,38 @@ public class LoopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loops);
         Toast.makeText(getApplicationContext(), "onCreat()", Toast.LENGTH_SHORT).show();
-        button2 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(LoopActivity.this,MainActivity.class);
-                startActivityForResult(it,0x123);
+                startActivityForResult(it,0x111);
+                finish();
                 setContentView(R.layout.activity_main);
+                Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
             }
         });
         Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
     }
+
+    // 定义一个变量，来标识是否退出
+    private static boolean isExit = false;
+    Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            isExit = false;
+        }
+    };
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (!isExit) {
+                isExit = true;
+                mHandler.sendEmptyMessageDelayed(0, 2000);
+            } else {
+                exit(R.layout.activity_loops);
+                Toast.makeText(getApplicationContext(), "onDestory()", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);}
 }
--- app/src/main/java/com/example/ranhaowen/hellowworld/MainActivity.java
+++ app/src/main/java/com/example/ranhaowen/hellowworld/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.ranhaowen.hellowworld;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -17,13 +16,18 @@ import static java.lang.System.exit;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "[hello]MainActivity: ";
+
     private Button btnchoose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d (TAG, " onCreate in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "onCreat()", Toast.LENGTH_SHORT).show();
+        Log.d (TAG, " onCreate out");
         btnchoose = (Button)findViewById(R.id.btnchoose);
+        Log.d (TAG, " Click in");
         btnchoose.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(MainActivity.this,LoopActivity.class);
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.activity_loops);
             }
         });
+
         Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
     }
 
@@ -50,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "再按一次退出程序",
                         Toast.LENGTH_SHORT).show();
                 mHandler.sendEmptyMessageDelayed(0, 2000);
-                Toast.makeText(getApplicationContext(), "onDestory()", Toast.LENGTH_SHORT).show();
             } else {
-                exit(0);
+                exit(R.layout.activity_main);
+                Toast.makeText(getApplicationContext(), "onDestory()", Toast.LENGTH_SHORT).show();
             }
             return false;
         }
